c445b882dc500050c92c14a782ba206abaef8496
Martin Wimpress <code@flexion.org>
2020-07-25 10:48:52 +0100

    Make power button script PEP 8 and 263 compliant
    Credit to https://github.com/kounch/argonone

--- argon1.sh
+++ argon1.sh
@@ -115,20 +115,30 @@ EOM
 # Generate script to monitor shutdown button
     cat <<'EOM' > "${powerbuttonscript}"
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# -*- mode: Python; tab-width: 4; indent-tabs-mode: nil; -*-
+# PEP 8, PEP 263.
+"""
+Argon One Fan and Button Service Daemon
+"""
+
 import smbus
 import RPi.GPIO as GPIO
 import os
 import time
 from threading import Thread
+
 rev = GPIO.RPI_REVISION
 if rev == 2 or rev == 3:
     bus = smbus.SMBus(1)
 else:
     bus = smbus.SMBus(0)
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-shutdown_pin=4
-GPIO.setup(shutdown_pin, GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
+shutdown_pin = 4
+GPIO.setup(shutdown_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 
 def shutdown_check():
     while True:
@@ -138,10 +148,11 @@ def shutdown_check():
         while GPIO.input(shutdown_pin) == GPIO.HIGH:
             time.sleep(0.01)
             pulsetime += 1
-        if pulsetime >=2 and pulsetime <=3:
+        if pulsetime >= 2 and pulsetime <= 3:
             os.system("reboot")
-        elif pulsetime >=4 and pulsetime <=5:
+        elif pulsetime >= 4 and pulsetime <= 5:
             os.system("shutdown now -h")
+
 
 def get_fanspeed(tempval, configlist):
     for curconfig in configlist:
@@ -151,6 +162,7 @@ def get_fanspeed(tempval, configlist):
         if tempval >= tempcfg:
             return fancfg
     return 0
+
 
 def load_config(fname):
     newconfig = []
@@ -181,20 +193,21 @@ def load_config(fname):
                         continue
                 except:
                     continue
-                newconfig.append( "{:5.1f}={}".format(tempval,fanval))
+                newconfig.append("{:5.1f}={}".format(tempval, fanval))
         if len(newconfig) > 0:
             newconfig.sort(reverse=True)
     except:
         return []
     return newconfig
 
+
 def temp_check():
     fanconfig = ["65=100", "60=55", "55=10"]
-    tmpconfig = load_config("'$daemonconfigfile'")
+    tmpconfig = load_config("/etc/argononed.conf")
     if len(tmpconfig) > 0:
         fanconfig = tmpconfig
-    address=0x1a
-    prevblock=0
+    address = 0x1a
+    prevblock = 0
     while True:
         temp = os.popen("cat /sys/class/thermal/thermal_zone0/temp").readline()
         val = float(int(temp)/1000)
@@ -203,14 +216,15 @@ def temp_check():
             time.sleep(30)
         prevblock = block
         try:
-            bus.write_byte(address,block)
+            bus.write_byte(address, block)
         except IOError:
-            temp=""
+            temp = ""
         time.sleep(30)
 
+
 try:
-    t1 = Thread(target = shutdown_check)
-    t2 = Thread(target = temp_check)
+    t1 = Thread(target=shutdown_check)
+    t2 = Thread(target=temp_check)
     t1.start()
     t2.start()
 except:
