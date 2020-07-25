fd9d7afd8447a079d26bd1cff18b00b21307a50b
Martin Wimpress <code@flexion.org>
2020-07-25 10:52:35 +0100

    Make systemd shutdown script PEP8 and 263 compliant
    Credit to https://github.com/kounch/argonone

--- argon1.sh
+++ argon1.sh
@@ -93,22 +93,30 @@ EOM
 
     # Generate script that runs every shutdown event
     cat <<'EOM' > "${shutdownscript}"
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# -*- mode: Python; tab-width: 4; indent-tabs-mode: nil; -*-
+# PEP 8, PEP 263.
+"""
+Argon Systemd Shutdown script
+"""
+
 import sys
 import smbus
 import RPi.GPIO as GPIO
+
 rev = GPIO.RPI_REVISION
 if rev == 2 or rev == 3:
     bus = smbus.SMBus(1)
 else:
     bus = smbus.SMBus(0)
-if len(sys.argv)>1:
-    bus.write_byte(0x1a,0)
+
+if len(sys.argv) > 1:
+    bus.write_byte_data(0x1a,0,0)
     if sys.argv[1] == "poweroff" or sys.argv[1] == "halt":
         try:
-            bus.write_byte(0x1a,0xFF)
+            bus.write_byte_data(0x1a,0,0xFF)
         except:
-            rev=0
+            rev = 0
 EOM
     chmod 755 "${shutdownscript}"
 
